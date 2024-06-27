0eab025c090ce8efa00028634883fd8e3fb8c70d
RoseMontoya <98787013+RoseMontoya@users.noreply.github.com>
2024-06-26 18:05:35 -0700

    created route to delete Group image

---
+++ backend/routes/api/group-images.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const { Sequelize, Op } = require('sequelize');
+
+const { requireAuth } = require('../../utils/auth');
+const { makeGroupObj, notFound, forbidden} = require('../../utils/helpers')
+const { Group, Membership, GroupImage, User, Venue } = require('../../db/models');
+
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const router = express.Router();
+
+// Delete an Image for a Group
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    // Search for Image
+    const image = await GroupImage.findByPk(req.params.imageId, {
+        include: [
+            {
+                //Include organizer from group
+                model: Group,
+                attributes: ['organizerId'],
+                include: [
+                    {
+                        // Going through group, get members with status of co-host
+                        model: Membership,
+                        where: {
+                            status: 'co-host'
+                        },
+                        attributes: ['userId'],
+                        required: false
+                    }
+                ]
+            }
+        ]
+    });
+
+    // Check user id matches one of the co-hosts
+    const cohosts = image.Group.Memberships;
+    let userCohost= false;
+    cohosts.forEach( cohost => {
+        if (req.user.id === cohost.userId) userCohost = true;
+    })
+
+    // Check if user is not the organizer or a co-host
+    if (req.user.id !== image.Group.organizerId && userCohost === false) return next(forbidden());
+    // Check if image was found
+    if (!image) return next(notFound('Group Image'));
+
+    // res.json(image)
+    // Destory image and return a success message
+    await image.destroy();
+    res.json({ "message": "Successfully deleted" })
+})
+
+
+
+module.exports = router;
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const groupsRouter = require('./groups.js')
+const groupsRouter = require('./groups.js');
+const groupImagesRouter = require('./group-images.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
@@ -13,7 +14,9 @@ router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
 
-router.use('/groups', groupsRouter)
+router.use('/groups', groupsRouter);
+
+router.use('/group-images', groupImagesRouter);
 
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
